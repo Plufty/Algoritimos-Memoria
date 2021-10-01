3d298e2e5be555cb067f5d7acaf30470d1fc94fd
Plufty <gleidson_dm@hotmail.com>
2021-10-01 17:27:43 -0300

    Second_chance

--- main.c
+++ main.c
@@ -54,10 +54,10 @@ int fifo(int8_t** page_table, int num_pages, int prev_page,
 
 int second_chance(int8_t** page_table, int num_pages, int prev_page,
                   int fifo_frm, int num_frames, int clock) 
-{ 
+{
     int page;
 
-    while(page < num_pages)//percorrendo todas as páginas    
+    for(page=0;page < num_pages; page++)//percorrendo todas as páginas    
     {
         if(page_table[page][PT_FRAMEID] == fifo_frm && page_table[page][PT_MAPPED]!=0 && page_table[page][PT_REFERENCE_BIT] == 0)//se essa foi a primeira moldura acessada e a página está mapeada, ela será substituída
         {            
@@ -65,21 +65,20 @@ int second_chance(int8_t** page_table, int num_pages, int prev_page,
         }                
         if(page_table[page][PT_FRAMEID] == fifo_frm && page_table[page][PT_MAPPED]!=0 && page_table[page][PT_REFERENCE_BIT] == 1)
         {
-            //devo colocar a página no final da fila
-            page_table[page][PT_REFERENCE_BIT] = 0;                        
-            if(fifo_frm < num_pages)
+            //mover para o final da fila;
+            page_table[page][PT_REFERENCE_BIT] = 0;
+            //Altera o a primeira moldura acessada para dar a segunda chance pra página
+            if(fifo_frm > num_frames)
             {
                 fifo_frm+=1;
             }
-            else
+            else if(fifo_frm >= num_frames)
             {
                 fifo_frm = 0;
             }
-            page = 0;
+            page-=1;
         }
-        page++;
-    }
-
+    }   
 }
 
 
